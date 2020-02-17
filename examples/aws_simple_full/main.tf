f2fee8b5e377c227680f3f18ab59e70e909d8806
Nikoleta Verbeck <nerdynick@gmail.com>
2020-02-17 12:07:58 -0700

    Add a simple example with at least 1 of everything

---
+++ examples/aws_simple_full/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+variable "first_name" {
+    type = string
+    description = "Your first name. NOTE: No special characters and all in lowercase."
+}
+variable "cluster_id" {
+    type = string
+    default = ""
+    description = "If you're wishing to have multiple clusters running at a single time. You can define a unique string here. NOTE: No special chars and all in lowercase."
+}
+variable "key_pair" {
+    type = string
+    description = "Your KeyPair Name that you have already added to AWS"  
+}
+
+
+module "aws_cp" {
+  source = "../../modules/aws"
+
+  zk_servers = 3
+  broker_servers = 3
+  c3_servers = 1
+  ksql_servers = 1
+  connect_servers = 1
+  rest_servers = 1
+  sr_servers = 1
+  first_name = var.first_name
+  cluster_id = var.cluster_id
+  key_pair = var.key_pair
+}
